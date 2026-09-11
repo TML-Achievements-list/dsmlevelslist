5835d40cdaa5393d60b6a290cf83e137097727b6
GDNoob402 <gmd.noo8@gmail.com>
2026-09-11 18:07:47 +1000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,9 +11,6 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
-        return 0;
-    }
     if (rank > 75 && percent < 100) {
         return 0;
     }
@@ -21,16 +18,17 @@ export function score(rank, percent, minPercent) {
     // Old formula
     /*
     let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+        ((percent - (minPercent - 100)) / (100 - (minPercent - 100)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 250) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 700) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
     if (percent != 100) {
         return round(score - score / 3);
+        minPercent = 100
     }
 
     return Math.max(round(score), 0);
